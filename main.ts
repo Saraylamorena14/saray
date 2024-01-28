2af407ac313b3a071831fbf91ab28972abca9466
Saraylamorena14 <157984956+Saraylamorena14@users.noreply.github.com>
2024-01-28 19:55:21 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,2 @@
-music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(440, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
+music.play(music.stringPlayable("C D A F G E B C5 ", 120), music.PlaybackMode.InBackground)
